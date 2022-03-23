f73c66939614a2b0334980f41f33c359fdec8d04
xtayex <75450574@qq.com>
2022-03-23 10:01:32 +0800

    default option will be selected when enter is pressed

--- util/so_launcher.sh
+++ util/so_launcher.sh
@@ -10,10 +10,10 @@ BOLD="\033[1m"
 BLUE="\033[34;5;11m"
 
 read -p $'\e[1m\e[31mWhat problem do you want to search?\e[0m: ' problem
-echo -e "\e[1m\e[31mWhich engine do you want to use?\e[0m"
+echo -e "\e[1m\e[31mWhich engine do you want to use? (input 1, 2 or 3)\e[0m"
 echo "1) Google (default)"
 echo "2) DuckDuckGo"
-echo "3) StackOverExchange"
+echo "3) StackExchange"
 
 engine='google'
 read n
@@ -21,7 +21,8 @@ case $n in
     1) engine='google';;
     2) engine='duckduckgo';;
     3) engine='stackexchange';;
-    *) echo 'invalid option'; exit;;
+    "") engine='google';;
+    *) echo 'invalid option';;
 esac
 echo $engine
 
